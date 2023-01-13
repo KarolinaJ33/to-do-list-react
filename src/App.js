30d03fc31c554a8b41fe2d01ef536b43cd280d27
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2023-01-13 12:19:42 +0100

    task list without interaction yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="body">
+
+    <header>
+        <h1 className="header">Lista zadań</h1>
+    </header>
+
+    <section className="section">
+        <div className="section__header">
+            <h2 className="section__title">Dodaj nowe zadanie</h2>
+        </div>
+        <form className="form js-form">
+            <input className="form__newTask js-newTask" placeholder="Co jest do zrobienia?" autofocus />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    </section>
+
+    <section className="section">
+        <div className="section__header">
+            <h2 className="section__title">Lista zadań</h2>
+            <div className="section__buttons js-buttons"></div>
+        </div>
+        <ul className="section__tasks js-tasks"></ul>
+    </section>
+
+</body>
   );
 }
 
