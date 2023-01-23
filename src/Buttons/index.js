0b30115f54a19a429e399e00c2305da70777f097
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2023-01-23 09:32:19 +0100

    corrections in the code

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,11 +4,11 @@ const Buttons = ({ tasks, hideDoneTasks}) => (
   <div className="buttons">
    {tasks.length > 0 && (
     <>
-      <button className="buttons__section">
+      <button className="buttons__button">
         {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
       </button>
       <button
-        className="buttons__section"
+        className="buttons__button"
         disabled={tasks.every(({ done }) => done)} 
       >
         Ukończ wszystkie
--- src/Section/index.js
+++ src/Section/index.js
@@ -6,9 +6,7 @@ const Section = ({ title, body, extraHeaderContent }) => (
            <h2 className="section__title">{title}</h2>
            {extraHeaderContent}
         </header>
-        <div className="section__body">
            {body}
-        </div>
     </section>
 );
 
