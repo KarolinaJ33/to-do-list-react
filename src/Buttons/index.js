a74383f5698198402e9ce1dbeaaca78effdccb90
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2023-01-23 09:17:00 +0100

    code formatting

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -8,8 +8,8 @@ const Buttons = ({ tasks, hideDoneTasks}) => (
         {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
       </button>
       <button
-      className="button__section"
-      disabled={tasks.every(({ done }) => done)} 
+        className="button__section"
+        disabled={tasks.every(({ done }) => done)} 
       >
         Ukończ wszystkie
       </button>
