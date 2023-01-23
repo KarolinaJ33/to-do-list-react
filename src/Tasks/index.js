18b7874bb39ba3b26959d0873d157a1cd9ec87d7
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2023-01-23 10:03:55 +0100

    formatted code

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,29 +1,30 @@
 import "./style.css";
 
 const Tasks = ({tasks, hideDoneTasks}) => (
-    <ul className="task">
-        {tasks.map(task => (
-            <li key={task.id}
-                className={`task__item${
-                    task.done && hideDoneTasks
-                     ? "task__item--hidden"
-                     : ""}`
-                }
-            >
-                 <button className="task__button task__button--done">
-                  {task.done ? "✓" : ""}
-              </button>
-              <span className={
-                `task__content${task.done ? " task__content--done " : ""}`
-              }>
-                  {task.content}
-              </span>
-              <button className="task__button task__button--remove">
-                  🗑
-              </button>
-            </li>
-    ))}
-    </ul>
+  <ul className="task">
+    {tasks.map(task => (
+      <li key={task.id}
+        className={`task__item${
+          task.done && hideDoneTasks
+          ? "task__item--hidden"
+          : ""}`
+          }
+      >
+        <button className="task__button task__button--done">
+          {task.done ? "✓" : ""}
+        </button>
+
+        <span className={
+          `task__content${task.done ? " task__content--done " : ""}`
+          }>
+          {task.content}
+        </span>
+        <button className="task__button task__button--remove">
+          🗑
+        </button>
+      </li>
+   ))}
+  </ul>
 );
 
 export default Tasks;
