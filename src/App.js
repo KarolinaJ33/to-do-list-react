35437f4a6324e4d2ec31e1cd2ab6fd8bbc51f48e
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2023-01-27 11:24:45 +0100

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -31,6 +31,13 @@ function App() {
     }));
   }
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true,
+    })));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -54,6 +61,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone} 
+            setAllDone={setAllDone}
           />
         }
       />  
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
   <div className="buttons">
    {tasks.length > 0 && (
      <>
@@ -8,6 +8,7 @@ const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
          {hideDone ? "Pokaż" : "Ukryj"} ukończone
        </button>
        <button
+         onClick={setAllDone}
          className="buttons__button"
          disabled={tasks.every(({ done }) => done)} 
        >
