86525f56b7bec7ec0903df89a761e8d6e3ce6bb7
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2023-01-27 13:16:29 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -38,12 +38,23 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length -1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={ <Form />}
+        body={ <Form addNewTask={addNewTask} />}
       />
 
       <Section
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,26 @@
+import React, { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <input className="form__newTask" placeholder="Co jest do zrobienia?" autofocus />
+const Form = ({addNewTask}) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+          <input
+            value={newTaskContent}
+            className="form__newTask"
+            placeholder="Co jest do zrobienia?"
+            onChange={({ target }) => setNewTaskContent(target.value)}
+        />
         <button className="form__button">Dodaj zadanie</button>
-    </form>
-);
+        </form>
+    );
+};
 
 export default Form;
