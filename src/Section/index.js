aa61f4e2bccfc1bfcbd940dbf002a51ddc79b831
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-08 11:18:52 +0100

    fixed css in Section and in index.css

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,12 +1,14 @@
 import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-  <section className="section section__body">
-    <header className="section__header">
-      <h2 className="section__title">{title}</h2>
+  <section className="section">
+    <header className="section__header--grid">
+      <h2 className="section__header--title">{title}</h2>
       {extraHeaderContent}
     </header>
-    {body}
+    <div className="section__body">
+      {body}
+    </div>
   </section>
 );
 
