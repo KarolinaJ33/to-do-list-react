682eeccafeee8ae187cb19fbfdbae26313f2401e
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-09 10:04:55 +0100

    formatted code

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import "./style.css";
 
-const Form = ({addNewTask}) => {
-    const [newTaskContent, setNewTaskContent] = useState("");
+const Form = ({ addNewTask }) => {
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        if (newTaskContent.trim() !== "") {    
-        addNewTask(newTaskContent.trim());
-        }
-        setNewTaskContent("");
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTaskContent.trim() !== "") {
+      addNewTask(newTaskContent.trim());
+    }
+    setNewTaskContent("");
+  };
 
-    return (
-        <form className="form" onSubmit={onFormSubmit}>
-          <input
-            value={newTaskContent}
-            className="form__newTask"
-            placeholder="Co jest do zrobienia?"
-            autoFocus
-            onChange={({ target }) => setNewTaskContent(target.value)}
-        />
-        <button className="form__button">Dodaj zadanie</button>
-        </form>
-    );
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        value={newTaskContent}
+        className="form__newTask"
+        placeholder="Co jest do zrobienia?"
+        autoFocus
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  );
 };
 
 export default Form;
