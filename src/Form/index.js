71260b25df0f04958fca8dfcab126e01c4741b6d
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-09 10:22:39 +0100

    added trim

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,7 +6,7 @@ const Form = ({ addNewTask }) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    if (newTaskContent === "") {
+    if (newTaskContent.trim() === "") {
       return;
     }
 
