449f537ac74e726cb65244a7008f908543584221
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-15 08:50:35 +0100

    removed example tasks from App

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ import Container from "./Container";
 function App() {
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState([
-    { id: 1, content: "przejść na Reacta", done: false }, 
-    { id: 2, content: "zjeść obiad", done: true },
-  ]);
+    ]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
