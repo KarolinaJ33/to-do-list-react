9c4d88b3d3490fc4f6858f71e24baae11c4a98fe
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-17 10:12:29 +0100

    modified code in initalState

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ import Container from "./Container";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(() => {
-
-    const savedTasks = localStorage.getItem("tasks");
-    return savedTasks ? JSON.parse(savedTasks) : [];
-  });
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
 
   useEffect (() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
