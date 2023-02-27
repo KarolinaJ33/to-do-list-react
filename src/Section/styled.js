adfec273c4e907d73d9a56dcff2ed787987e1d25
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-27 11:58:10 +0100

    ThemeProvider added

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const Wrapper = styled.section`
     background: ${({ theme }) => theme.color.white};
