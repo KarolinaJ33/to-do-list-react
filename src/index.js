9434775732f6b812f208b25f2695f8b5774b0bfb
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-22 09:06:28 +0100

    import Provider

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 import { GlobalStyle } from "./GlobalStyle";
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
