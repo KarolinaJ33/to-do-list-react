474bfba7d99ea7a19c6a08b62e926b4736d4ca62
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-22 09:04:30 +0100

    toggleHideDone changed to Redux

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -4,14 +4,18 @@ const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
     tasks: [],
+    hideDone: false,
   },
   reducers: {
     addTask: ({ tasks }, { payload }) => {
       tasks.push(payload);
     },
+    toggleHideDone: state => {
+      state.hideDone = !state.hideDone;
+    },
   },
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, toggleHideDone } = tasksSlice.actions;
 export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
