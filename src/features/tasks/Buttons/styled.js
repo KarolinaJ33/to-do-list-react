6a1fff543bdaecafe7eff588b439967bce7f6545
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-24 10:25:21 +0100

    align buttons

--- src/features/tasks/Buttons/styled.js
+++ src/features/tasks/Buttons/styled.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    padding: 20px;
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    
+    @media(max-width: 767px) {
          display: flex;
          flex-direction: column;
          align-items: center;
@@ -16,18 +15,22 @@ export const Button = styled.button`
     border: none;
     margin: auto;
     cursor: pointer;
-    padding: 10px;
+    padding: 20px;
     transition: filter 0.3s;
 
-    &:hover{
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        padding: 10px;
+    }
+
+    &:hover {
      filter: brightness(110%);
     }
 
-    &:active{
+    &:active {
      filter: brightness(120%);  
     }
 
-    &:disabled{
+    &:disabled {
      color: ${({ theme }) => theme.color.silver};
      cursor: not-allowed;
     }
