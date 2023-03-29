9c4c1abc5b9d12bc0a1a6a4314cafdc89c85e0ad
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-29 12:29:24 +0200

    rootSaga added

---
+++ src/rootSaga.js
@@ -0,0 +1,9 @@
+import {all} from "redux-saga/effects";
+import {watchFetchExampleTasks} from "./features/tasks/tasksSaga";
+
+export default function* rootSaga() {
+    yield all([
+        watchFetchExampleTasks(),
+    ]);
+    
+}
