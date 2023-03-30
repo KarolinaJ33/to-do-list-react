2163d08e159060f9e7a8d705d23817377b3be863
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-30 12:55:55 +0200

    created Author.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+import { HashRouter, Link, Switch, Route, Redirect } from 'react-router-dom';
 import Tasks from './features/tasks/Tasks';
+import Author from './features/author/Author';
 
-export default () => <Tasks />;
+export default () => (
+    <HashRouter>
+    <nav>
+        <ul>
+            <li>
+                <Link to="/zadania">Zadania</Link>
+            </li>
+            <li>
+                <Link to="/autor">O autorze</Link>
+            </li>
+        </ul>
+        <Switch>
+            <Route path="/zadania">
+                <Tasks />
+            </Route>
+            <Route path="/autor">
+                <Author />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
+    </nav>
+    </HashRouter>
+);
