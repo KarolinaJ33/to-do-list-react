ad866fcd6c8031a71ca6f6094b0181e0cda6cf1a
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-04-03 09:13:46 +0200

    added TasksPage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import { HashRouter, NavLink, Switch, Route, Redirect } from 'react-router-dom';
-import TasksPage from "./features/tasks/TasksPage/index"
-import TaskPage from './features/tasks/TaskPage';
-import AuthorPage from './features/author/AuthorPage';
+import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage/index";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
 
 export default () => (
     <HashRouter>
-    <nav>
-        <ul>
-            <li>
-                <NavLink activeClassName="active" to="/zadania">Zadania</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" to="/autor">O autorze</NavLink>
-            </li>
-        </ul>
-        <Switch>
-            <Route path="/zadania/:id">
-                <TaskPage />
-            </Route>
-            <Route path="/autor">
-                <AuthorPage />
-            </Route>
-            <Route path="/">
-                <Redirect to="/zadania" />
-            </Route>
-        </Switch>
-    </nav>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink activeClassName="active" to="/zadania">
+                        Zadania
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/autor">
+                        O autorze
+                    </NavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/zadania">
+                    <TasksPage />
+                </Route>{" "}
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </nav>
     </HashRouter>
 );
