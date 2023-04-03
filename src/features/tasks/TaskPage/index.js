349e9807d03bf8469d2e8a5efe27c9ba386dfbc9
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-04-03 11:29:22 +0200

    useParams hook, task status

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,14 +1,22 @@
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import { Container } from "../../../common/Container/styled";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
-    const params = useParams();
+    const { id } = useParams();
+    const task = useSelector((state) => getTaskById(state, id));
+
     return (
         <Container>
             <Header title="Szczegóły zadania" />
-            <Section title={""} body={<>nana</>} />
+            <Section 
+                title={task ? task.content : "Nie znaleziono zadania😥"} 
+                body={<><strong> Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>
+                } 
+            />
         </Container>
     );
 }
