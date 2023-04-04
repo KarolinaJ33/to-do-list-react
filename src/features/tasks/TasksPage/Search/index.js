5d6140bf2a29644ecd8b6e1afe96e9a1c1e3d2a2
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-04-04 12:58:16 +0200

    Search component created

---
+++ src/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,32 @@
+import { useHistory, useLocation } from "react-router-dom";
+import Input from "../../Input";
+import { Wrapper } from "./styled";
+import searchQueryParamName from "../searchQueryParamName";
+
+export default () => {
+    const location = useLocation();
+    const history = useHistory();
+    const query = new URLSearchParams(location.search).get(searchQueryParamName);
+
+    const onInputChange = ({ target }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (target.value.trim() === "") {
+            searchParams.delete(searchQueryParamName);
+        } else {
+            searchParams.set(searchQueryParamName, target.value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString}`);
+    };
+
+    return (
+        <Wrapper>
+            <Input
+                placeholder="Filtruj zadania"
+                value={query || ""}
+                onChange={onInputChange}
+            />
+        </Wrapper>
+    );
+};
