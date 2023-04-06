49ea0dfbc399c22487495fd8e3df89a518b3b023
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-04-06 10:14:32 +0200

    added new import to handle errors - fetchExampleTasksSuccess and setTasks replaced with fetchExampleTasksSuccess

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,5 +1,5 @@
 import { takeEvery, call, put, takeLatest, select, delay } from "redux-saga/effects";
-import { fetchExampleTasks, setTasks, selectTasks } from "./tasksSlice";
+import { fetchExampleTasks, selectTasks, fetchExampleTasksSuccess, fetchExampleTasksError } from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTasks";
 import { saveTasksInLocalStorage } from "./tasksLocalStorage";
 
@@ -7,8 +7,9 @@ function* fetchExampleTasksHandler() {
     try {
         yield delay(1000);
         const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks(exampleTasks))
+        yield put(fetchExampleTasksSuccess(exampleTasks))
     } catch (error) {
+        yield put(fetchExampleTasksError());
         yield call(alert, "Coś poszło nie tak!");
     }
 }
