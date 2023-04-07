01a0a14c0204157fefbcdd4b59fc7bd814c84b01
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-04-07 09:51:02 +0200

    formatted code

--- src/appStyled.js
+++ src/appStyled.js
@@ -3,19 +3,19 @@ import styled from "styled-components";
 const activeClassName = "active";
 
 export const StyledNavLink = styled(NavLink)`
-text-decoration: none;
-color: ${({ theme }) => theme.color.white};
+    text-decoration: none;
+    color: ${({ theme }) => theme.color.white};
 
-&.${activeClassName} {
-    font-weight: bold;
-}
+    &.${activeClassName} {
+        font-weight: bold;
+    }
 `;
 export const Navigation = styled.ul`
-list-style: none;
-display: flex;
-justify-content: center;
-grid-gap: 30px;
-margin:0;
-padding: 15px;
-background: ${({ theme }) => theme.color.teal};
-`
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    grid-gap: 30px;
+    margin: 0;
+    padding: 15px;
+    background: ${({ theme }) => theme.color.teal};
+`;
